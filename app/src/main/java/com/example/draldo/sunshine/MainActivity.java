6c038a4a3ac5ecb8f54b7b8b400d4dc202c4a80f
Draldo <draldo_lopez@hotmail.com>
2016-08-07 18:14:18 -0400

    Sunshine with JUnit

--- app/src/main/java/com/example/draldo/sunshine/MainActivity.java
+++ app/src/main/java/com/example/draldo/sunshine/MainActivity.java
@@ -7,6 +7,7 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,6 +23,12 @@ public class MainActivity extends AppCompatActivity {
                     .add(R.id.container, new ForecastFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 
     @Override
