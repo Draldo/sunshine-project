fcfdd2cc5e0b235e50401779ac502de6bf6df636
Draldo <draldo_lopez@hotmail.com>
2016-08-16 21:49:32 -0400

    Lesson 5

--- app/src/main/java/com/example/draldo/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/draldo/sunshine/ForecastAdapter.java
@@ -106,6 +106,8 @@ public class ForecastAdapter extends CursorAdapter {
         String description = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
         viewHolder.descriptionView.setText(description);
 
+        viewHolder.iconView.setContentDescription(description);
+
         boolean isMetric = Utility.isMetric(context);
 
         double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
